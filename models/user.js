e9274f878689c6ee8b4e286fc4769c3173dc2c72
ebadillo23 <eliseo.badillo@nbcuni.com>
2015-02-23 18:20:08 -0800

    Created front-end for adding new users.

--- models/user.js
+++ models/user.js
@@ -3,7 +3,31 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   name: String,
-	sso: Number
+  sso: String,
+  pin: String
 });
+
+userSchema.statics.findBySSO = function(sso, callBack) {
+  this.findOne({
+    sso: sso
+  }, callBack);
+}
+
+userSchema.statics.saveNewUser = function(name, sso, pin, callBack) {
+  if (!(name && name.length)) {
+    return callBack(new Error('Invalid name'));
+  }
+  if (!(sso && sso.length && sso.length === 9 && !isNaN(parseInt(sso)))) {
+    return callBack(new Error('Invalid SSO'));
+  }
+  if (!(pin && pin.length && pin.length === 4 && !isNaN(parseInt(pin)))) {
+    return callBack(new Error('Invalid PIN'));
+  }
+  this.create({
+    name: name,
+    sso: sso,
+    pin: pin
+  }, callBack);
+}
 
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -2,31 +2,63 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+var renderCreateNewUserPage = function(res, locals) {
+  var allLocals = {
+    title: 'Add a New Gatekeeper User',
+    error: null,
+    user: null
+  };
+  if (locals) {
+    for (prop in locals) {
+      if (Object.prototype.hasOwnProperty.call(locals, prop)) {
+        allLocals[prop] = locals[prop];
+      }
+    }
+  }
+  res.render('users/add_new_user', locals);
+}
+
+// list all users
+router.route('/all')
+  .all(function(req, res, next) {
+    User.find({}, function(err, users) {
+      if (err) {
+        return console.error(err);
+      }
+      if (users.length) {
+        res.send(users);
+      } else {
+        res.send("No users found!");
+      }
+    });
+  });
+
 // verify if a user is registered by sso.
 router.get('/:sso(\\d{9})/', function(req, res, next) {
-  var sso = req.params.sso;
-  User.findOne({sso: sso}, function(err, user) {
+  User.findBySSO(req.params.sso, function(err, user) {
     if (err) {
-      console.err(err);
+      console.error(err);
     }
     if (user) {
-      res.status(200).end();
+      res.status(200).send();
     } else {
-      res.status(404).end();
+      res.status(404).send();
     }
   });
 });
 
-router.get('/insertTestUser', function(req, res, next) {
-  User.create({
-      name: 'Testy McTesterson',
-      sso: 123456789
-    }, function(err, user) {
-      if (err) {
-        console.error(err);
+// front-end route for creating a new user
+router.route('/addNewUser')
+  .get(function(req, res, next) {
+    renderCreateNewUserPage(res);
+  })
+  .post(function(req, res, next) {
+    var reqBody = req.body;
+    User.saveNewUser(reqBody.name, reqBody.sso, reqBody.pin,
+      function(err, user) {
+        renderCreateNewUserPage(res, { error: err, user: user });
       }
-      res.send("Testy McTesterson has been entered.");
-    });
-});
+    );
+  });
 
 module.exports = router;
